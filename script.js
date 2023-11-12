2827cd9bcd06e9857b6724074e8daa2956d86767
Nicopobletee <149529377+Nplandry@users.noreply.github.com>
2023-11-12 01:02:53 -0300

    Update script.js
    Nueva actualización al js en la cual se implementa la funcion de deslizar cada 10 segundo el carrusel, cuando no se tiene el cursor sobre el mismo.
    Pendiente: añadir los comentarios finales sobre todo el proyecto.

--- script.js
+++ script.js
@@ -1,6 +1,5 @@
 const punto = document.querySelectorAll(".punto")
 const carruselContent = document.querySelector(".carrusel-content")
-
 punto.forEach( (Puntos, i)=> {
     punto[i].addEventListener("click", ()=> {
     let posicion = i
@@ -12,3 +11,35 @@ punto.forEach( (Puntos, i)=> {
     punto[i].classList.add('activo')
 })
 })
+
+document.addEventListener('DOMContentLoaded', function () {
+    var carousel = document.querySelector('.carrusel');
+    var carouselInner = document.querySelector('.carrusel-content');
+
+    var currentIndex = 0;
+    var totalItems = document.querySelectorAll('.carousel-item').length; 
+    var itemWidth = document.querySelector('.carousel-item').offsetWidth;
+
+    function nextSlide() {
+        currentIndex = (currentIndex + 1) % totalItems;
+        updateCarousel();
+    }
+
+    function updateCarousel() {
+        var translateValue = -currentIndex * itemWidth;
+        carouselInner.style.transform = 'translateX(' + translateValue + 'px)';
+    }
+
+    // Configurar intervalo para cambiar automáticamente de diapositiva cada 3 segundos
+    var intervalId = setInterval(nextSlide, 10000);
+
+    // Detener el intervalo cuando el mouse entra en el carrusel
+    carousel.addEventListener('mouseenter', function () {
+        clearInterval(intervalId);
+    });
+
+    // Reanudar el intervalo cuando el mouse sale del carrusel
+    carousel.addEventListener('mouseleave', function () {
+        intervalId = setInterval(nextSlide, 10000);
+    });
+});
